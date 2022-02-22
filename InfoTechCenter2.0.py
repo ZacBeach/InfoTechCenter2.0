f3d8c38acf6952f54f89432e51049b77f4491af9
ZacBeach <02zachary.beach@gulllakecs.org>
2022-02-22 13:45:28 -0500

    Add color

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,8 +1,10 @@
 # Code Name - Hornet
 
 from time import sleep #You can use Python’s sleep() function to add a time delay to your code.
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Welcome to Hornets InfoTechCenter")
+print(Fore.GREEN + "Welcome to Hornets InfoTechCenter")
 sleep(1)
-
 print("\nHornet's Operating System Booting up")
