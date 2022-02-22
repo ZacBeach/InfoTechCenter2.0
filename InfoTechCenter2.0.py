10adc0f303e468cc51339686136766e5d641550c
ZacBeach <02zachary.beach@gulllakecs.org>
2022-02-22 13:29:15 -0500

    Import colorama

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,5 +1,8 @@
 # Code Name - Hornet
 
+from time import sleep #You can use Python’s sleep() function to add a time delay to your code.
+
 print("Welcome to Hornets InfoTechCenter")
+sleep(1)
 
 print("\nHornet's Operating System Booting up")
