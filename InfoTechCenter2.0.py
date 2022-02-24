679cd6ff2abe7d2048f74ddf5a4ecbd6180838d5
ZacBeach <02zachary.beach@gulllakecs.org>
2022-02-24 13:41:47 -0500

    Weather function declared and stable

---
+++ InfoTechCenter2.0.py
@@ -0,0 +1,13 @@
+import random
+
+# Weather Branch
+
+def weather():
+    weatherForecast = ["Icy","Snowy","Raining","Windy","Foggy","Sunny"]
+    weatherConditions = random.choice(weatherForecast)
+    return weatherConditions
+
+# calling weather function to determine weather
+weatherAlert = weather()
+
+print(weatherAlert)
